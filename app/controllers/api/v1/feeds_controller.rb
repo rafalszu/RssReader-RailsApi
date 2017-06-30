310faca847e2276c1549f7439d08f0d465a2110f
Rafal <rafal@szumilas.org>
2017-06-30 16:25:14 +0000

    last devise bits

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -1,6 +1,8 @@
 # https://sourcey.com/building-the-prefect-rails-5-api-only-app/
 
 class Api::V1::FeedsController < ApplicationController
+    
+    
     def index
         render json: Feed.all
     end
