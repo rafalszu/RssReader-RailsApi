9f4f19abcaf505c77b1f34841517ef800a07d13d
Rafal <rafal@szumilas.org>
2017-06-30 15:10:40 +0000

    moved to versioned api

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
   
-  resources :feeds
-  resources :entries
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
+  namespace :api do
+    namespace :v1  do
+      devise_for :users
+      
+      resources :feeds
+      resources :entries
+    end
+  end
 end
