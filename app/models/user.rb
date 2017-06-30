13fab5cbf75edb6d29b37ce57fcbe2d369b3aa73
Rafal <rafal@szumilas.org>
2017-06-30 14:57:20 +0000

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  has_many :feeds
+  has_many :entries
+  
+  validates_presence_of :email, :password
+  validates_format_of :email, :with => /.+@.+\..+/i
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should have_many(:feeds) }
+    it { should have_many(:entries) }
+  end
+  
+  describe 'Validations' do
+    let(:validemail) { 'dummy@addr.com' }
+    let(:notvalidemail) { 'totally-not-a-valid-email-address' }
+    secret = 'nottellin'
+    subject { described_class.new }
+    
+    it 'is valid with valid attributes' do
+      subject.email = validemail
+      subject.password = secret
+      
+      expect(subject).to be_valid
+    end
+    
+    it 'is NOT valid without an email' do
+      subject.email = nil
+      subject.password = secret
+      expect(subject).to_not be_valid
+    end
+    
+    it 'is NOT valid without a password' do
+      subject.email = validemail
+      subject.password = nil
+      
+      expect(subject).to_not be_valid
+    end
+    
+    it 'is NOT valid for a nonvalid email' do
+      subject.email = notvalidemail
+      subject.password = secret
+      
+      expect(subject).to_not be_valid
+    end
+    
+    it 'is valid for a tagged+ email' do
+      subject.email = 'valid+addr@dummy.com'
+      subject.password = secret
+      
+      expect(subject).to be_valid
+    end
+  end
+  
 end
