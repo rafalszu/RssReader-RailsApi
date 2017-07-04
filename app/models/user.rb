6fd17a89aa13dd564385c4728df50289c403b8ef
Rafal <szumilas@gmail.com>
2017-07-04 22:24:44 +0200

    changed rcket notation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :feeds
   has_many :entries
   has_one :login
-  
-  validates_format_of :email, :with => /.+@.+\..+/i
+
+  validates_format_of :email, with: /.+@.+\..+/i
 end
