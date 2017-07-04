4551ea7a7d259673986d4b771df3c7ce3a8fc319
Rafal <szumilas@gmail.com>
2017-07-05 00:01:56 +0200

    uri helper method name change

--- app/helpers/uri_helper.rb
+++ app/helpers/uri_helper.rb
@@ -2,15 +2,22 @@ require 'uri'
 
 module UriHelper
   module_function
-  
-  def is_valid_url?(url = '')
+
+  def valid_url?(url = '')
     return false if url.blank? || url.split('.').length < 2
-    
+
     url = "http://#{url}" unless url.start_with?('http', 'https')
     uri = URI.parse(url)
-    
+
     uri.kind_of? URI::HTTP
   rescue URI::InvalidURIError
     false
+  end
+
+  def from_string(url = '')
+    return nil unless valid_url?(url)
+
+    url = "http://#{url}" unless url.start_with?('http', 'https')
+    URI.parse(url)
   end
 end
