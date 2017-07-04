2697694b5a0d8259145cc06b65252b9ef49c3b01
Rafal <rafal@szumilas.org>
2017-07-04 15:49:02 +0000

    reordered methods

--- app/helpers/rss_helper.rb
+++ app/helpers/rss_helper.rb
@@ -2,15 +2,18 @@ require 'uri_helper'
 module RssHelper
   module_function
   
+  def latest_entries_from_content(content = '')
+    return [] if content.blank?
+
+    
+  end
+  
   def latest_entries_from_url(url = '')
     byebug
     content = get_content_from_url(url)
     return [] if content.blank?
-  end
-
-  def latest_entries_from_content(content = '')
-    return [] if content.blank?
-
+    
+    latest_entries_from_content(content)
   end
 
   def get_content_from_url(url = '')
