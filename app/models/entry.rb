a0fa3796898c42764dd49b2dd2b2d0a79af3e0ae
Rafal <szumilas@gmail.com>
2017-07-04 23:16:18 +0200

    entries updated from feed

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,6 +5,18 @@ class Entry < ApplicationRecord
 
   belongs_to :feed
   belongs_to :user
-    
+
   validates_presence_of :title, :permanent_url, :feed
+
+  def self.update_from_feed(feedjira_rssentries, feed)
+    return if feed.nil?
+
+    feedjira_rssentries.each do |rss_entry|
+      Entry.find_or_create_by!(permanent_url: rss_entry.url, user_id: feed.user.id, feed_id: feed.id) do |entry|
+        entry.read = false
+        entry.title = rss_entry.title
+        entry.content = rss_entry.summary
+      end
+    end
+  end
 end
