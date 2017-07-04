b3ac2f30ad26e4b3151cb2cc74b6911a6548348f
Rafal <szumilas@gmail.com>
2017-07-04 23:25:59 +0200

    added string sanitization

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -14,9 +14,9 @@ class Entry < ApplicationRecord
     feedjira_rssentries.each do |rss_entry|
       Entry.find_or_create_by!(permanent_url: rss_entry.url, user_id: feed.user.id, feed_id: feed.id) do |entry|
         entry.read = false
-        entry.title = rss_entry.title
-        entry.summary = rss_entry.summary
-        entry.content = rss_entry.content
+        entry.title = rss_entry.title.sanitize
+        entry.summary = rss_entry.summary.sanitize
+        entry.content = rss_entry.content.sanitize
         entry.author = rss_entry.author
         entry.published = rss_entry.published
       end
