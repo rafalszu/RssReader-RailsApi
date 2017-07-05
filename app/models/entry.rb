bf38ab7e39482662a56f0ca3cb16531198f3a74c
Rafal <szumilas@gmail.com>
2017-07-05 23:29:10 +0200

    Feed can now update rss entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -8,18 +8,4 @@ class Entry < ApplicationRecord
 
   validates_presence_of :title, :permanent_url, :feed
 
-  def self.update_from_feed(feedjira_rssentries, feed)
-    return if feed.nil?
-
-    feedjira_rssentries.each do |rss_entry|
-      Entry.find_or_create_by!(permanent_url: rss_entry.url, user_id: feed.user.id, feed_id: feed.id) do |entry|
-        entry.read = false
-        entry.title = rss_entry.title.sanitize
-        entry.summary = rss_entry.summary.sanitize
-        entry.content = rss_entry.content.sanitize
-        entry.author = rss_entry.author
-        entry.published = rss_entry.published
-      end
-    end
-  end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,10 +1,27 @@
 require "deleteable"
+require 'rss_helper'
 
 class Feed < ApplicationRecord
   include Deletable
-  
+
   has_many :entries
   belongs_to :user
-    
+
   validates_presence_of :title, :address
+
+  def update_from_feed
+    return if self.id.nil? || self.address.blank?
+
+    rss_entries = RssHelper.latest_entries_from_url(self.address)
+    rss_entries.each do |rss_entry|
+      Entry.find_or_create_by!(permanent_url: rss_entry.url, user_id: self.user.id, feed_id: self.id) do |entry|
+        entry.read = false
+        entry.title = rss_entry.title
+        entry.summary = rss_entry.summary
+        entry.content = rss_entry.content
+        entry.author = rss_entry.author
+        entry.published = rss_entry.published
+      end
+    end
+  end
 end
