599b5b22b33e9600e4979ff10c7814e4cf6c2970
Rafal <szumilas@gmail.com>
2017-07-09 22:55:28 +0200

    FeedsController first tet

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::FeedsController, type: :controller do
-
+  it 'ensure it derives from AuthenticatedController' do
+    expect(FeedsController).to be < AuthenticatedController
+  end
 end
