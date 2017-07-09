9f32075c048416795b2d8bb5b0714221c7d1d3b2
Rafal <szumilas@gmail.com>
2017-07-09 23:09:04 +0200

    updated namespace for users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe Api::V1::UsersController, type: :controller do
   it 'ensure it derives from AuthenticatedController' do
     expect(UsersController).to be < AuthenticatedController
   end
