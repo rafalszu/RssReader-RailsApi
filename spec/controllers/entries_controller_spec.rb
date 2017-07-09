98f462159c2143da80aa59b6bc03c660010ac869
Rafal <szumilas@gmail.com>
2017-07-09 22:56:41 +0200

    entriesController first test

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::EntriesController, type: :controller do
-
+  it 'ensure it derives from AuthenticatedController' do
+    expect(EntriesController).to be < AuthenticatedController
+  end
 end
