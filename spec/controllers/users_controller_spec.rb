07ba786949a8d345cb968dbdf67249fdebed5115
Rafal <szumilas@gmail.com>
2017-07-19 00:06:32 +0200

    commented out posting to /token test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,11 +14,23 @@ RSpec.describe Api::V1::UsersController, type: :api do
     u.create_login(identification: johns_email, password: johns_password, password_confirmation: johns_password)
   end
 
-  it 'returns token after passing all required params' do
-    post_params = { grant_type: 'password', username: johns_email, password: johns_password }
-    post '/token', post_params
+  # describe 'POST /token' do
+  #   let(:params) { { grant_type: 'password', username: johns_email, password: johns_password } }
+  #   subject { post '/token', params: params, headers: { 'HTTPS' => false } }
 
-    puts last_response.body
-    true
-  end
+  #   context 'for grant_type = "PASSWORD"' do
+  #     it 'responds with status 200' do
+  #       subject
+
+  #       puts subject
+  #       expect(last_response).to have_http_status(200)
+  #     end
+
+  #     it 'responds with an access token' do
+  #       subject
+
+  #       expect(last_response.body).to be_json_eql({ access_token: login.oauth2_token }.to_json)
+  #     end
+  #   end
+  # end
 end
